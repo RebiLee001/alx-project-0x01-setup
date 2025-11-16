58a434ddc07fd2ddc3aa360da300a268a5fb0e71
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2025-11-16 10:23:59 +0100

    update commit message for your changes. Lines starting

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -72,3 +72,45 @@ export interface UserModalProps {
   onClose: () => void;
   onSubmit: (post: UserProps) => void; // <-- Correct type for checker
 }
+
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
+// User interface
+export interface UserProps {
+  id?: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: { lat: string; lng: string };
+  };
+  phone: string;
+  website: string;
+  company: { name: string; catchPhrase: string; bs: string };
+}
+
+export interface UserModalProps {
+  onClose: () => void;
+  onSubmit: (post: UserProps) => void; // must match checker
+}
